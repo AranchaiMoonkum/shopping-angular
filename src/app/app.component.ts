05dad018f54a35f631a992183508f8f9a9c6a690
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-03-26 09:49:14 +0700

    get api from a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { ApiService } from "./services/api.service"
 
 @Component({
     standalone: false,
@@ -7,4 +8,14 @@ import { Component } from "@angular/core"
     styleUrl: "./app.component.scss"
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    products: any[] = []
+
+    constructor(private apiService: ApiService) { }
+
+    ngOnInit(): void {
+        this.apiService.getProducts().subscribe((data: any) => {
+            this.products = data
+        })
+    }
+}
