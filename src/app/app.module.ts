8af1c7332198038b262f2321a334efc57f8eb320
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-03-26 09:47:52 +0700

    added http modules in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from "@angular/core"
 import { BrowserModule, provideClientHydration } from "@angular/platform-browser"
 
 import { AppRoutingModule } from "./app-routing.module"
-import { AppComponent } from "./app.component";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { AppComponent } from "./app.component"
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async"
+import { HttpClientModule, provideHttpClient, withFetch } from "@angular/common/http"
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule
     ],
-    providers: [provideClientHydration(), provideAnimationsAsync()],
+    providers: [provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())],
     bootstrap: [AppComponent]
 })
 
