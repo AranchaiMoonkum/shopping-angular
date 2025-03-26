31fb952479d1b5c638eb4d52dbe81aaa0c3f3614
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-03-26 11:54:37 +0700

    decorated and show data from api into cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
+// import angular core and platform modules
 import { NgModule } from "@angular/core"
 import { BrowserModule, provideClientHydration } from "@angular/platform-browser"
 
+// import routing module
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
+
+// import http and animations modules
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async"
 import { HttpClientModule, provideHttpClient, withFetch } from "@angular/common/http"
+
+// import ui components
+import { MatCardModule } from "@angular/material/card"
+import { MatDividerModule } from "@angular/material/divider"
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        MatCardModule,
+        MatDividerModule
     ],
     providers: [provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())],
     bootstrap: [AppComponent]
