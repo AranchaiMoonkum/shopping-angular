56970cf245a324ff4373617b2b20fe04df1c41c1
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-03-28 09:28:11 +0700

    created add to cart button

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,17 +1,18 @@
 // import components
 import { HomeComponent } from "./home/home.component"
-import { MatCardModule } from "@angular/material/card"
 
 // import modules
 import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
+import { MatCardModule } from "@angular/material/card"
+import { MatIconModule } from "@angular/material/icon"
 
 // import functional components
 import { ProductFilterModule } from "../product-filter/product-filter.module"
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, ProductFilterModule, MatCardModule],
+    imports: [CommonModule, ProductFilterModule, MatCardModule, MatIconModule],
     exports: [HomeComponent],
 })
 export class HomeModule {}
