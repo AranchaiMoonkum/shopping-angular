58657c59680e608d472d30c2f48503b39d647834
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-04-03 15:21:38 +0700

    modified checkout-dialog.component.html

--- src/app/components/checkout-dialog/checkout-dialog.component.ts
+++ src/app/components/checkout-dialog/checkout-dialog.component.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from "@angular/common"
-import { Component, Inject, OnInit } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 
 import {
     MatDialogModule,
     MatDialogTitle,
     MatDialogContent,
+    MatDialogRef,
 } from "@angular/material/dialog"
 import { Product } from "../../types/interface"
 import { MatButtonModule } from "@angular/material/button"
 import { CartService } from "../../services/cart.service"
 import { FormsModule } from "@angular/forms"
-import { MatSnackBarModule } from "@angular/material/snack-bar"
+import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar"
 import { MatIconModule } from "@angular/material/icon"
 import { MatTooltipModule } from "@angular/material/tooltip"
 import { Observable } from "rxjs"
@@ -36,7 +37,11 @@ export class CheckoutDialogComponent implements OnInit {
     cart$!: Observable<Product[]>
     totalPrice$!: Observable<number>
 
-    constructor(private cartService: CartService) {}
+    constructor(
+        private cartService: CartService,
+        public dialogRef: MatDialogRef<CheckoutDialogComponent>,
+        private _snackBar: MatSnackBar
+    ) {}
 
     ngOnInit(): void {
         this.cart$ = this.cartService.getCart()
@@ -45,5 +50,15 @@ export class CheckoutDialogComponent implements OnInit {
 
     removeProductFromCart(product: Product) {
         this.cartService.removeProductFromCart(product)
+    }
+
+    checkoutCart() {
+        this._snackBar.open("Checkout successful", "Close", {
+            duration: 2000,
+        })
+    }
+
+    onClose() {
+        this.dialogRef.close()
     }
 }
