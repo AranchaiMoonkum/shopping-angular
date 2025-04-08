fd6ba7d4dc052d801b7a218eb3883dd9d4e44ef5
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-04-08 17:12:21 +0700

    changed values of options

--- src/app/components/product-filter/product-filter/product-filter.component.ts
+++ src/app/components/product-filter/product-filter/product-filter.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core"
 
+type Sort = "asc" | "desc" | "none"
+
 @Component({
     standalone: false,
     selector: "app-product-filter",
@@ -10,11 +12,11 @@ export class ProductFilterComponent implements OnInit {
     @Input() categories: string[] = []
     @Output() filterChange = new EventEmitter<{
         category: string
-        sort: "asc" | "desc" | "none"
+        sort: Sort
     }>()
 
     selectedCategory: string = "all"
-    selectedSort: "asc" | "desc" | "none" = "none"
+    selectedSort: Sort = "none"
 
     ngOnInit(): void {
         this.emitFilters()
