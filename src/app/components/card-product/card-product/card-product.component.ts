b548d358f24e4d704e14a0be7629bc280d4b3a50
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-04-24 11:52:17 +0700

    feat: added cart service and used cart service in card product

--- src/app/components/card-product/card-product/card-product.component.ts
+++ src/app/components/card-product/card-product/card-product.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from "@angular/core"
 import { Product } from "../../../types/interface"
 import { ProductService } from "../../../services/product.service"
+import { CartService } from "../../../services/cart.service"
 
 @Component({
     selector: "app-card-product",
@@ -11,7 +12,10 @@ import { ProductService } from "../../../services/product.service"
 export class CardProductComponent {
     @Input() product!: Product
 
-    constructor(private readonly productService: ProductService) {}
+    constructor(
+        private readonly productService: ProductService,
+        private readonly cartService: CartService
+    ) {}
 
     getStarPercentage(rate: number): string {
         return `${(rate / 5) * 100}%`
@@ -19,5 +23,13 @@ export class CardProductComponent {
 
     getTimeStamp() {
         return this.productService.getTimeStamp()
+    }
+
+    getProductQuantity(product: Product): number {
+        return this.cartService.getProductQuantity(product)
+    }
+
+    onQuantityChange(quantity: number): void {
+        this.cartService.updateProductQuantity(this.product, quantity)
     }
 }
