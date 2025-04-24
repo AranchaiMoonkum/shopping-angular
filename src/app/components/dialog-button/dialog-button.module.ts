805133e986c458b3b1fbe3c9e012504899eed8b0
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-04-24 13:09:38 +0700

    feat: added dialog button

---
+++ src/app/components/dialog-button/dialog-button.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DialogButtonComponent } from './dialog-button/dialog-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    DialogButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DialogButtonModule { }
