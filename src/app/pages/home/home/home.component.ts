fb8e74f10ed38c4d03076005a80f3be8c0a63313
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-04-24 09:58:49 +0700

    refractory: moved product codes to show product component

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,7 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core"
-import { ProductService } from "../../../services/product.service"
-import { Product } from "../../../types/interface"
-import { ApiService } from "../../../services/api.service"
+import { ChangeDetectionStrategy, Component } from "@angular/core"
 
 @Component({
     selector: "app-home",
@@ -9,73 +6,4 @@ import { ApiService } from "../../../services/api.service"
     styleUrl: "./home.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
-    products: Product[] = []
-    productsDisplay: Product[] = []
-    categories: string[] = []
-
-    selectedCategory: string = "all"
-
-    private readonly sortFunctions = {
-        none: () => 0,
-        "price-asc": (a: Product, b: Product) => a.price - b.price,
-        "price-desc": (a: Product, b: Product) => b.price - a.price,
-        "name-asc": (a: Product, b: Product) => a.title.localeCompare(b.title),
-        "name-desc": (a: Product, b: Product) => b.title.localeCompare(a.title),
-    }
-
-    constructor(
-        private readonly apiService: ApiService,
-        private readonly productService: ProductService
-    ) {}
-
-    ngOnInit(): void {
-        this.apiService
-            .getProducts()
-            .subscribe((data: { products: Product[] }) => {
-                this.productService.setProducts(data.products)
-                this.products = data.products
-
-                this.productsDisplay = this.products
-
-                this.categories = [
-                    "all",
-                    ...this.products
-                        .map((p) => p.category)
-                        .filter((value, index, self) => {
-                            return self.indexOf(value) === index
-                        }),
-                ]
-            })
-    }
-
-    onFilterChange(filters: {
-        category: string
-        sort: string
-        search: string
-    }) {
-        if (filters.category === "all") {
-            this.productsDisplay = [...this.products]
-        } else {
-            this.productsDisplay = this.products.filter(
-                (product) => product.category === filters.category
-            )
-        }
-
-        const sortFn =
-            this.sortFunctions[filters.sort as keyof typeof this.sortFunctions]
-        if (sortFn) {
-            this.productsDisplay.sort(sortFn)
-        }
-
-        if (filters.search) {
-            this.productsDisplay = this.productsDisplay.filter((product) =>
-                product.title
-                    .toLowerCase()
-                    .includes(filters.search.toLowerCase())
-            )
-        }
-
-        return this.productsDisplay
-    }
-}
+export class HomeComponent {}
