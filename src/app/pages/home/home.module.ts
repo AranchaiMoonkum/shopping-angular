dcffb077c79cde7d7dfbf4e7a060af3547ce9d49
Aranchai Moonkum <moonkum.aranchai@gmail.com>
2025-04-29 13:41:19 +0700

    refactor: improve documentation in HomeModule for clarity and maintainability

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,22 +1,28 @@
-// import components
 import { HomeComponent } from "./home/home.component"
-
-// import modules
 import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
 import { NavbarModule } from "../../components/navbar/navbar.module"
-
-// import angular material modules
 import { MatCardModule } from "@angular/material/card"
 import { MatIconModule } from "@angular/material/icon"
 import { MatSnackBarModule } from "@angular/material/snack-bar"
-
-// import functional components
 import { ProductFilterModule } from "../../components/product-filter/product-filter.module"
 import { UpdateQuantityModule } from "../../components/update-quantity/update-quantity.module"
 import { ShowProductModule } from "../../components/show-product/show-product.module"
 import { DialogButtonModule } from "../../components/dialog-button/dialog-button.module"
 
+/**
+ * Home feature module containing the main shopping page components.
+ * 
+ * This module:
+ * - Provides the main HomeComponent
+ * - Imports required Angular Material UI components
+ * - Integrates custom feature modules for the shopping experience
+ * - Encapsulates the product display, filtering, and cart functionality
+ * 
+ * Dependencies:
+ * - Angular Material: Card, Icon, and SnackBar components for UI
+ * - Custom feature modules: Navbar, ProductFilter, UpdateQuantity, ShowProduct, and DialogButton
+ */
 @NgModule({
     declarations: [HomeComponent],
     imports: [
